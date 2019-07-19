e0e85717d94d240f5f9c84f00d51afb56cdd7d41
Renato Cugler <renatocugler@gmail.com>
2019-07-19 10:42:41 -0500

    limited # of api calls

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,23 +4,17 @@ const express = require('express');
 const unirest = require('unirest');
 
 const router = express.Router();
-//const Article = require('../models/article').default;
-//const Incident = require('../models/incident').default.;
 
 module.exports = (app) => {
   app.use('/', router);
 };
 
-
 router.get('/lat/:lat/long/:long', (req, res, next) => {
 
   //TODO - check input parameters
-  console.log("Input params latitude - "+ req.params.lat + ", longitude - "+ req.params.long)
   var url = 'https://bikewise.org:443/api/v2/incidents?page=1&incident_type=theft&proximity='+req.params.lat +' %2C'+req.params.long +'&proximity_square=2'
-
   console.log("API call: "+ url )
   unirest.get(url)
-  //unirest.get('https://bikewise.org:443/api/v2/incidents?page=1&incident_type=theft&proximity=50.11913%2C8.63877&proximity_square=2')
 
   .end(function (response) {
 
@@ -28,86 +22,52 @@ router.get('/lat/:lat/long/:long', (req, res, next) => {
     var incidents = data.incidents;
 
     var count = data.incidents.length;
-    console.log("Incidents reported by the API: "+ count);
+    //console.log("Incidents reported by the API: "+ count);
 
-    var safety_style;
-    var safety_message;
+    var style;
+    var message;
+    var description;
 
     switch(true)
     {
       case (count == 0 ):
-          safety_style = 'my-content-neutral';
-          safety_message= 'Enjoy your ride!';
-          safety_description = '';
+          style = 'my-content-neutral';
+          message= 'Enjoy your ride!';
+          description = '';
         break;
       case (count < '2'):
-          safety_style = 'my-content-yellow';
-          safety_message = 'WARNING';
-          safety_description = 'A bike was reported stolen in this region.'
+          style = 'my-content-yellow';
+          message = 'WARNING';
+          description = 'A bike was reported stolen in the region.'
         break;
       default:
-          safety_style = 'my-content-red';
-          safety_message = 'AVOID THIS AREA';
-          safety_description =  count + ' bikes reported stolen in this region';
+          style = 'my-content-red';
+          message = 'AVOID THIS AREA';
+          description =  count + ' Bikes reported stolen in the region';
         break;
     }
-
-    res.render('bike-theft', {
-      title: 'Stolen Bikes near you',
-      message: 'Waiting for COBI.Bike to load...',
-      safety_message: safety_message,
-      safety_style: safety_style,
-      safety_description:safety_description,
-      incidents: incidents
+    res.send({
+      display_message: message,
+      display_style: style,
+      display_description: description
     });
     
   });
 });
 
+//router.param('lat', function (req, res, next, id) {
+  //req.display_message = "NEW";
+//  console.log("abcd");
+//})
+
+
+//app.use(router)
 
 router.get('/', (req, res, next) => {
-  
-  unirest.get('https://bikewise.org:443/api/v2/incidents?page=1&incident_type=theft&proximity=50.11913%2C8.63877&proximity_square=2')
-
-  .end(function (response) {
-
-    var data= response.body;
-    var incidents = data.incidents;
-
-    var count = data.incidents.length;
-    console.log("Incidents reported by the API: "+ count);
-
-    var safety_style;
-    var safety_message;
-
-    switch(true)
-    {
-      case (count == 0 ):
-          safety_style = 'my-content-neutral';
-          safety_message= 'Enjoy your ride!';
-          safety_description = '';
-        break;
-      case (count < '2'):
-          safety_style = 'my-content-yellow';
-          safety_message = 'WARNING';
-          safety_description = 'A bike was reported stolen in this region.'
-        break;
-      default:
-          safety_style = 'my-content-red';
-          safety_message = 'AVOID THIS AREA';
-          safety_description =  count + ' bikes reported stolen in this region';
-        break;
-    }
-
-    res.render('bike-theft', {
+     res.render('bike-theft', {
       title: 'Stolen Bikes near you',
       message: 'Waiting for COBI.Bike to load...',
-      safety_message: safety_message,
-      safety_style: safety_style,
-      safety_description:safety_description,
-      incidents: incidents
-      
+      safety_style: "my-content-neutral"
     });
     
-  });
 });
